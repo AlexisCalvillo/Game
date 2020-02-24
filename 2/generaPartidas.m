f21f511c070d3b663e77543d08b4f2cced4a1de6
Alexis Calvillo <acalvillm@gmail.com>
2020-02-24 15:55:23 -0600

    Eliminación de archivos innecesarios

--- 2/generaPartidas.m
+++
@@ -1,6 +0,0 @@
-function [vot] = generaPartidas(nJug, minVot, maxVot)
-%Genera partidas aleatorias de n jugadores y un número máximo y mínimo 
-%de votantes
-vot =minVot+randi(maxVot, 1,nJug);
-
-end
